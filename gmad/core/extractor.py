0cf77284dd350424caa78565eb3daddde3d61699
sunset <aidanstewart@sunsetdeveloper.com>
2021-01-08 23:39:24 -0500

    Initial commit

--- gmad/core/extractor.py
+++ gmad/core/extractor.py
@@ -6,7 +6,7 @@ def extract(parent_dir, delete=False):
         if '.gma' in f:
             file_to_extract = parent_dir + '/' + f
             os.system('lib/gmad_linux extract -file {0}'.format(file_to_extract))
-            print('-----------------------------------------------------------------------------------------------------\n')
+            print('-------------------------------------------------------------------------------------------------\n')
             if delete:
                 os.remove(file_to_extract)
 
