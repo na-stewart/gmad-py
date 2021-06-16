9248f2d4f2d049a5fa2079fae2247299cf01170c
Sunset Dev <aidanstewart@sunsetdeveloper.com>
2021-06-15 23:13:43 -0400

    Update gmad.py

--- gmad.py
+++ gmad.py
@@ -1,5 +1,22 @@
-from gmad.core.creator import create
-from gmad.core.extractor import extract
+import os
+
+
+def extract(parent_dir, delete=False):
+    for f in os.listdir(parent_dir):
+        if '.gma' in f:
+            file_to_extract = parent_dir + '/' + f
+            os.system('lib/gmad_linux extract -file {0}'.format(file_to_extract))
+            if delete:
+                os.remove(file_to_extract)
+
+
+def create(parent_dir, delete=False):
+    for d in os.listdir(parent_dir):
+        if '.gma' not in d:
+            folder_to_create = parent_dir + '/' + d
+            os.system('lib/gmad_linux create -folder {0} -out {1}'.format(folder_to_create, folder_to_create + '.gma'))
+            if delete:
+                os.rmdir(folder_to_create)
 
 
 def user_input():
@@ -16,8 +33,7 @@ def execute(parent_dir, delete, gmad_type):
         create(parent_dir, delete == 'yes')
     else:
         raise TypeError('Gmad execution type must be extract or create.')
-    print ('Thanks for using GmadEEC. Have a great day and I hope to see you soon!')
-
+    print('Thanks for using GmadEEC. Have a great day and I hope to see you soon!')
 
 
 if __name__ == '__main__':
